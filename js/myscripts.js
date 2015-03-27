f03a1ede842080ebac3656ca447be134cdc1362b
sushilcs111 <sushil.sud@gmail.com>
2015-03-28 00:44:38 +0530

    latest

--- js/myscripts.js
+++ js/myscripts.js
@@ -68,6 +68,66 @@ $(document).ready(function () {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
+    
+     $("#wrapper").on('click','.addToCart',function (e) {
+        e.preventDefault();
+        var formId = $(this).attr("form-id");
+       $.ajaxSetup({
+            scriptCharset: "utf-8", //maybe "ISO-8859-1"
+            contentType: "application/json; charset=utf-8"
+        });
+
+            $.post($(this).attr('form-action'), $("form#" + formId).serialize(), function (result) {
+
+                var cart_cont = result.split("||||||");
+
+                var cart = $('.shake-cart');
+                var imgtodrag = $("form#" + formId).find("img").eq(0);
+                if (imgtodrag) {
+                    var imgclone = imgtodrag.clone()
+                            .offset({
+                                top: imgtodrag.offset().top,
+                                left: imgtodrag.offset().left
+                            })
+                            .css({
+                                'opacity': '0.5',
+                                'position': 'absolute',
+                                'height': '150px',
+                                'width': '150px',
+                                'z-index': '100'
+                            })
+                            .appendTo($('body'))
+                            .animate({
+                                'top': cart.offset().top + 10,
+                                'left': cart.offset().left + 10,
+                                'width': 75,
+                                'height': 75
+                            });
+
+
+
+
+
+                    imgclone.animate({
+                        'width': 0,
+                        'height': 0
+                    }, function () {
+
+                        $(this).detach();
+                        cart.effect("shake", {
+                            times: 2
+                        }, 200);
+
+                        $(".shopping_cart").html(cart_cont[0]);
+                    });
+                }
+
+
+            });
+        
+
+    });
+
 });
 
 
