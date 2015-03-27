21dbfc4774e30c1f757c66c77063a1ffc0fd96f6
sushilcs111 <sushil.sud@gmail.com>
2015-03-27 23:37:21 +0530

    new

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,36 +1,53 @@
 var domain = "http://www.pluckk.com/";
 
-var app = angular.module('pluckk', ['ngResource','ngSanitize']);
+var app = angular.module('pluckk', ['ngResource', 'ngSanitize']);
 
 
-app.controller('productList', function ($scope, $http) {
-    var res;
-    var url = domain + "m/get-category-products/shop";
+app.controller('getMenu', function ($scope, $http) {
+    var url = domain + "m/get-menu";
 
     $.ajaxSetup({
         scriptCharset: "utf-8", //maybe "ISO-8859-1"
         contentType: "application/json; charset=utf-8"
     });
 
-
-
-
-
     $.getJSON('http://whateverorigin.org/get?url=' +
             encodeURIComponent(url) + '&callback=?',
             function (data) {
 
                 $scope.$apply(function () {
-                    
-                    
-                    $scope.products = data.contents;
+                    $scope.menu = data.contents;
                 });
             });
 
 });
+app.controller('productList', function ($scope, $http) {
 
-app.filter('unsafe', function($sce) {
-    return function(val) {
+    $scope.products = [];
+
+    $scope.getProds = function (url) {
+        console.log(url);
+        $.ajaxSetup({
+            scriptCharset: "utf-8", //maybe "ISO-8859-1"
+            contentType: "application/json; charset=utf-8"
+        });
+
+        $.getJSON('http://whateverorigin.org/get?url=' +
+                encodeURIComponent(url) + '&callback=?',
+                function (data) {
+                    $scope.$apply(function () {
+                        $scope.products = data.contents;
+                    });
+                });
+    }
+
+
+});
+
+
+
+app.filter('unsafe', function ($sce) {
+    return function (val) {
         return $sce.trustAsHtml(val);
     };
 });
@@ -42,6 +59,10 @@ app.filter('unsafe', function($sce) {
 
 $(document).ready(function () {
 
+    $(".sidebar-nav").on('click', 'a', function (e) {
+        e.preventDefault();
+        angular.element($("#pList")).scope().getProds($(this).attr('href'));
+    });
 
     $("#menu-toggle").click(function (e) {
         e.preventDefault();
